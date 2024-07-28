bbe884bd4f28b93e6eaff222df66c8e363ad4d0a
sid <sidorov@mail.ioffe.ru>
2024-07-28 15:25:04 +0200

    Added controls

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
-
+// Camera and scene
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 100 );
 camera.position.set( 1, 2, - 3 );
 camera.lookAt( 0, 1, 0 );
@@ -12,6 +13,8 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0x555555 );
 scene.fog = new THREE.Fog( 0xa0a0a0, 10, 50 );
 
+// Lights
+
 // const light = new THREE.AmbientLight( 0x404040 ); // soft white light
 // scene.add( light );
 
@@ -19,7 +22,7 @@ scene.fog = new THREE.Fog( 0xa0a0a0, 10, 50 );
 // hemiLight.position.set( 0, 0, 20 );
 // scene.add( hemiLight );
 
-const dirLight = new THREE.DirectionalLight( 0xffffff, 3 );
+const dirLight = new THREE.DirectionalLight( 0xffffff, 15 );
 dirLight.position.set( - 3, 10, - 10 );
 dirLight.castShadow = true;
 dirLight.shadow.camera.top = 2;
@@ -30,14 +33,52 @@ dirLight.shadow.camera.near = 0.1;
 dirLight.shadow.camera.far = 40;
 scene.add( dirLight );
 
+// Objects (Models)
+
 //scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
 
 // ground
-
-const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 1, 1 ), new THREE.MeshPhongMaterial( { color: 0xdddddd, depthWrite: false } ) );
+const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 1, 1 ), new THREE.MeshPhongMaterial( { color: 0x555555, depthWrite: false } ) );
 mesh.rotation.x = - Math.PI / 2;
-// mesh.receiveShadow = true;
+mesh.receiveShadow = true;
 scene.add( mesh );
+
+//GUI
+const gui = new GUI();
+gui.title('Robot Kuka kr10');
+// const folder_pos = gui.addFolder('Position');
+const folder_ctrl = gui.addFolder('Control');
+const robot_pose = {
+    A1: 0.,
+    A2: 0.,
+    A3: 0.,
+    A4: 0.,
+    A5: 0.,
+    A6: 0.,
+    reset: function (){
+        this.A1 = 0;
+        this.A2 = 0;
+        this.A3 = 0;
+        this.A4 = 0;
+        this.A5 = 0;
+        this.A6 = 0;
+    }
+}
+// folder_pos.add(robot_pose, "A1").listen().decimals(3);
+// folder_pos.add(robot_pose, "A2").listen().decimals(3);
+// folder_pos.add(robot_pose, "A3").listen().decimals(3);
+// folder_pos.add(robot_pose, "A4").listen().decimals(3);
+// folder_pos.add(robot_pose, "A5").listen().decimals(3);
+// folder_pos.add(robot_pose, "A6").listen().decimals(3)   ;
+
+folder_ctrl.add(robot_pose,"A1",-170, 170).listen();
+folder_ctrl.add(robot_pose,"A2",-190,45).listen();
+folder_ctrl.add(robot_pose,"A3",-120,156).listen();
+folder_ctrl.add(robot_pose,"A4",-185,185).listen();
+folder_ctrl.add(robot_pose,"A5",-120,120).listen();
+folder_ctrl.add(robot_pose,"A6",-350,350).listen();
+folder_ctrl.add(robot_pose,"reset");
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -58,26 +99,18 @@ const elbow = arm.children[0];
 const forearm = elbow.children[0];
 const hand = forearm.children[0];
 
-// loader.load( 'kr10.glb', function ( gltf ) {
-//     const base = gltf.scene.children[0];
-//     const shoulder = base.children[0];
-//
-//     scene.add( gltf.scene );
-//     console.log(gltf)
-//     console.log(shoulder)
-//
-// }, undefined, function ( error ) {
-//
-//     console.error( error );
-//
-// } );
 
 
 
 function animate() {
     requestAnimationFrame( animate );
-    shoulder.rotation.y += 0.01;
-    forearm.rotation.x -= 0.01;
+    shoulder.rotation.y = robot_pose.A1 * Math.PI / 180;
+    arm.rotation.z = robot_pose.A2 * Math.PI / 180;
+    elbow.rotation.z = robot_pose.A3 * Math.PI / 180;
+    forearm.rotation.x = robot_pose.A4 * Math.PI / 180;
+    hand.rotation.z = robot_pose.A5 * Math.PI / 180;
+
+    // forearm.rotation.x -= 0.01;
     controls.update();
     renderer.render( scene, camera );
 
